ac88c55155caacfa607c7c46f4cefa998d38589f
Ryan Montoya <ryan@ledgerinvesting.com>
2019-08-20 20:59:07 -0400

    Vertically centers brand in nav bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -20,12 +20,14 @@ const NAV_LINKS = [
 
 const Header = () => (
   <header className={styles.root}>
-    <nav className={styles.navContainer}>
-      <h1 className={styles.title}>
-        <Link to="/" className={styles.titleLink}>
-          Ryan Montoya
-        </Link>
-      </h1>
+    <nav className={styles.nav}>
+      <div className={styles.brand}>
+        <h1 className={styles.title}>
+          <Link to="/" className={styles.titleLink}>
+            Ryan Montoya
+          </Link>
+        </h1>
+      </div>
       <div className={styles.links}>
         {NAV_LINKS.map((link) => (
           <Link
